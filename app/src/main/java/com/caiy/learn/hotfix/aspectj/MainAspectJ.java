12cb51ba4077028a89c308ef63c25f3875918bbf
caiyong <caiyong@corp.netease.com>
2018-08-14 17:11:16 +0800

    update MainAspectJ

--- app/src/main/java/com/caiy/learn/hotfix/aspectj/MainAspectJ.java
+++ app/src/main/java/com/caiy/learn/hotfix/aspectj/MainAspectJ.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
 
 /**
  * Created by caiyong on 2018/8/13.
@@ -27,20 +28,37 @@ public class MainAspectJ {
 
     }
 
-    @Before("executionMain()")
-    public void aroundExecutionMain(JoinPoint joinPoint){
+    @Around("executionMain()")
+    public void aroundExecutionMain(ProceedingJoinPoint joinPoint) throws Throwable {
         Log.i(TAG,String.format("joinPoint.toString()=%s",joinPoint.toString()));
         Log.i(TAG,String.format("joinPoint.toShortString()=%s",joinPoint.toShortString()));
         Log.i(TAG,String.format("joinPoint.toLongString()=%s",joinPoint.toLongString()));
         Log.i(TAG,String.format("joinPoint.getThis()=%s",joinPoint.getThis().toString()));
         Log.i(TAG,String.format("joinPoint.getTarget()=%s",joinPoint.getTarget().toString()));
         Log.i(TAG,String.format("joinPoint.getArgs()=%s",convertArgsToString(joinPoint.getArgs())));
-        Log.i(TAG,String.format("joinPoint.getSignature()=%s",joinPoint.getSignature().toString()));
+        Log.i(TAG,String.format("joinPoint.getSignature().toString()=%s",joinPoint.getSignature().toString()));
+        Log.i(TAG,String.format("joinPoint.getSignature().getParameterTypes()=%s",
+                convertClassArrayToString(((CodeSignature) joinPoint.getSignature()).getParameterTypes())));
         Log.i(TAG,String.format("joinPoint.getSignature().getDeclaringType()=%s",joinPoint.getSignature().getDeclaringType().toString()));
         Log.i(TAG,String.format("joinPoint.getSourceLocation()=%s",joinPoint.getSourceLocation().toString()));
         Log.i(TAG,String.format("joinPoint.getKind()=%s",joinPoint.getKind()));
 
-//        Log.i(TAG,String.format("target=%s,signature.name=%s",joinPoint.getTarget().toString(),joinPoint.getSignature().getName()));
+        joinPoint.proceed();
+    }
+
+    private String convertClassArrayToString(Class[] parameterTypes) {
+        StringBuilder builder = new StringBuilder();
+        if(parameterTypes != null){
+            int length = parameterTypes.length;
+            for(int i=0;i<length;i++){
+                Class parameterType = parameterTypes[i];
+                builder.append(parameterType.getCanonicalName());
+                if(i < length-1) {
+                    builder.append(",");
+                }
+            }
+        }
+        return builder.toString();
     }
 
     private String convertArgsToString(Object[] args){
